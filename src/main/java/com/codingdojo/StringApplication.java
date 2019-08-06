713f9f79c5295491c7d3dc1c1ea45af706e1b844
German Bernal <ger.e.bernal@gmail.com>
2019-08-06 10:51:34 -0500

    Add files via upload

---
+++ src/main/java/com/codingdojo/StringApplication.java
@@ -0,0 +1,26 @@
+package com.codingdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String Hi() {
+		return "Hello client! How are you doing?";
+	}
+	
+	@RequestMapping("/random")
+	public String spring() {
+		return "Spring Boot is great! So easy to just respond with strings";
+	}
+
+}
